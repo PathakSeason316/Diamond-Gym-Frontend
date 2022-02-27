f00ee3b0a77c61889ef31121e94783a6097a1855
SEASON PATHAK <seasonpathak12345@gmail.com>
2022-02-27 21:20:13 +0545

    comm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
-function App() {
+import LandingPage from './components/LandingPage'
+import LoginPage from './components/LoginPage'
+import HomePage from './components/HomePage'
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path ="/" component={LandingPage} />
+          <Route path = "/login" component = {LoginPage} />
+          <Route path = "/home" component = {HomePage} />
+        </Switch>
     </div>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
