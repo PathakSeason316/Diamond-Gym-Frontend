be445f38e58bbc8769b107e5b29f2304adf48bae
SEASON PATHAK <seasonpathak12345@gmail.com>
2022-03-03 16:20:40 +0545

    commit 3

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 export default function HomePage() {
     return (
         <div className="text-center">
-            <h1 className="main-title home-page-title">welcome to our app</h1>
+            <h1 className="main-title home-page-title">Welcome </h1>
             <Link to="/">
-                <button className="primary-button">Log out</button>
+                <button className="logout-button">Log out</button>
             </Link>
         </div>
     )
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import "../App.css";
 import {ReactComponent as AdminImage} from '../assets/AdminImage.svg'
 
+
 const LoginPage = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <div class="split left">
-        <h1 class="welcome">Welcome to</h1>
-        <h2 class="welcome">Diamond Gym Monitoring System!!</h2>
-        <div class="centered">
+        <h1 class="welcome1">Welcome to</h1>
+        <h2 class="welcome2">Diamond Gym Monitoring System!!</h2>
+        <div class="centered1">
           <AdminImage/>
         </div>
       </div>
 
       <div class="split right">
-        <div class="centered"></div>
+        <div class="centered2">
+          <form className ="login_form">
+            <input type="username" placeholder="Username" value={username} onChange={(e) => setUserName(e.target.value)}/>
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <button type="submit" className="submit_btn">Login</button>
+          </form>
+        </div>
+        <footer class="footer">@2021 Diamond Gym and Fitness</footer>
       </div>
     </>
   );
